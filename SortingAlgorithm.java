b198f842984d188b8a2317b3936d0e679727cf81
smartcatgrey <138059204+smartcatgrey@users.noreply.github.com>
2023-06-29 10:15:36 +0300

    Create SortingAlgorithm.java

---
+++ SortingAlgorithm.java
@@ -0,0 +1,15 @@
+// Implement a sorting algorithm in Java
+
+public class SortingAlgorithm {
+  public static void bubbleSort(int[] arr) {
+    // Implement bubble sort algorithm
+    // ...
+  }
+
+  // Implement more sorting algorithms like selection sort, merge sort, etc.
+  // ...
+}
+
+// Usage:
+// int[] array = ...; // Provide an array of elements
+// SortingAlgorithm.bubbleSort(array);
